f9618bc560e8f045eec71c153f6378d316f9ce99
Weizhe Yuan <50064641+yyy-Apple@users.noreply.github.com>
2021-07-25 10:10:14 +0800

    Add files via upload

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="result">Reaction Time: {{ reactionTime }} ms. </div>
+  <p class="rank"> {{ rank }} </p>
+</template>
+
+<script>
+export default {
+    props: ['reactionTime'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        console.log('Reulst mounted.')
+        if (this.score < 250) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    }
+}
+</script>
+
+<style>
+.result {
+    width: 400px;
+    border-radius: 5px;
+    background: rgb(157, 169, 184);
+    color: white;
+    text-align: center;
+    margin: 40px auto;
+}
+
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>

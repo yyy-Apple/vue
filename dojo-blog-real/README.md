d7b78e155bfdf9124e4a48410820149681359428
Weizhe Yuan <50064641+yyy-Apple@users.noreply.github.com>
2021-07-29 17:07:57 +0800

    Add files via upload

---
+++ dojo-blog-real/README.md
@@ -0,0 +1,21 @@
+# dojo-blog-real
+
+<img width=700px src="./src/assets/demo.png">
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ dojo-blog-real/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+
+    <!-- <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">Delete a post</button> -->
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+import PostList from "../components/PostList.vue";
+import getPosts from "../composables/getPosts.js";
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: { PostList },
+  setup() {
+    // const showPosts = ref(true);
+    const { posts, error, load } = getPosts();
+    load();
+
+    return { posts, error };
+  },
+};
+</script>
+
+
+
